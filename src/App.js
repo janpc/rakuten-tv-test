7f5db3de02df17e21349b1c7a19b55ec4806b403
janpc <janpuigcapella@gmail.com>
2025-05-05 19:49:47 +0200

    Feat: crate routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
