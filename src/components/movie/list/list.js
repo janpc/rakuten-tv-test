bfdce94b0312b5b1465348a72def7b61f9f88082
janpc <janpuigcapella@gmail.com>
2025-05-09 18:13:49 +0200

    Feat: full list component in desktop

--- src/components/movie/list/list.js
+++ src/components/movie/list/list.js
@@ -1,10 +1,20 @@
 import { use, useRef, useState } from 'react';
-import { MoviesContainer, Title, Wrapper } from './listStyles';
+import {
+  ArrowButton,
+  MoviesContainer,
+  Title,
+  Wrapper,
+} from './listStyles';
 import MovieListItem from '../item/item';
+import arrowLeft from '../../../assets/images/arrow-left.svg';
+import arrowRight from '../../../assets/images/arrow-right.svg';
 
 export default function List({ listPromise }) {
   const { name, movies } = use(listPromise);
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [isFullLeft, setIsFullLeft] = useState(true);
+  const [isFullRight, setIsFullRight] = useState(false);
+  const [hover, setHover] = useState(false);
   const moviesContainerRef = useRef();
 
   function scrollListLeft() {
@@ -16,6 +26,9 @@ export default function List({ listPromise }) {
       x = maxScroll;
     }
 
+    setIsFullRight(x === maxScroll);
+    setIsFullLeft(false);
+
     setScrollPosition(x);
   }
 
@@ -26,14 +39,36 @@ export default function List({ listPromise }) {
       x = 0;
     }
 
+    setIsFullLeft(x === 0);
+    setIsFullRight(false);
+
     setScrollPosition(x);
   }
 
   return (
-    <Wrapper>
+    <Wrapper
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <Title>{name}</Title>
-      <button type="button" onClick={scrollListLeft}>left</button>
-      <button type="button" onClick={scrollListRight}>right</button>
+      <ArrowButton
+        type="button"
+        onClick={scrollListLeft}
+        $position="left"
+        $display={hover && !isFullRight}
+        disabled={isFullRight}
+      >
+        <img alt="Move left arrow" src={arrowLeft} />
+      </ArrowButton>
+      <ArrowButton
+        type="button"
+        onClick={scrollListRight}
+        $position="right"
+        $display={hover && !isFullLeft}
+        disabled={isFullLeft}
+      >
+        <img alt="Move right arrow" src={arrowRight} />
+      </ArrowButton>
       <MoviesContainer $translate={scrollPosition} ref={moviesContainerRef}>
         {
           movies.map((movie) => (
--- src/components/movie/list/listStyles.js
+++ src/components/movie/list/listStyles.js
@@ -8,10 +8,12 @@ export const Title = styled.h1`
   font-weight: bold;
   color: var(--main-color);
   text-align: left;
+  margin-bottom: 1.2rem;
 `;
 
 // Create a Wrapper component that'll render a <section> tag with some styles
 export const Wrapper = styled.section`
+  position: relative;
   padding: 0 var(--main-margin);
   margin-bottom: 50px;
 `;
@@ -23,4 +25,23 @@ export const MoviesContainer = styled.div`
   overflow: hidden;
   transform: translateX(${(props) => props.$translate}px);
   transition: transform 0.5s ease-in-out;
+`;
+
+export const ArrowButton = styled.button`
+  position: absolute;
+  top: 1.2rem;
+  bottom: 0;
+  color: white;
+  ${(props) => props.$position}: 0;
+  background-color: red;
+  min-width: var(--main-margin);
+  z-index: 1;
+  background: linear-gradient(to ${(props) => props.$position}, transparent 0%, var(--bg-color) 90%);
+  border: none;
+
+  ${(props) => (!props.disabled && 'cursor: pointer;')}
+
+  img {
+    display: ${(props) => (props.$display ? 'block' : 'none')};
+  }
 `;
