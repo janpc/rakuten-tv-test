181d0fccf0cf489c98956b02a951559157bf784d
janpc <janpuigcapella@gmail.com>
2025-05-11 13:42:49 +0200

    Feat: add link to error logo

--- src/components/common/error-message/error-message.js
+++ src/components/common/error-message/error-message.js
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import rakutenLogo from '../../../assets/images/rakutentv.png';
 import { Container, Logo, Message } from './error-message.styles';
 
 export default function ErrorMessage({ children }) {
   return (
     <Container>
-      <Logo src={rakutenLogo} alt="Rakuten Logo" />
+      <Link to="/">
+        <Logo src={rakutenLogo} alt="Rakuten Logo" />
+      </Link>
       <Message>{children || 'Something went wrong!'}</Message>
     </Container>
   );
